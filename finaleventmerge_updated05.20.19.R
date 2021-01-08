17e8633840692d3505044018d29ab359be35c9fa
Brittany Manning <brittanymanning@pa-dhcp-10-120-227-45.gp-vpn.northwestern.private>
2021-01-08 16:00:31 -0600

    Jan 8 Weekly Update

--- finaleventmerge_updated05.20.19.R
+++ finaleventmerge_updated05.20.19.R
@@ -17,8 +17,8 @@ from_int <- int_all[str_detect(int_all, paste0(subids, collapse = "|"))]
 from_int <- from_int[str_detect(from_int, ".csv")]
 
 
-subid <- "W2W_3233"
-context <- "movie1"
+subid <- "SOC_800"
+context <- "book"
 ## Change this next line only if you will be timelocking to something other than the '0' time marker
 timelock <- 4
 
@@ -87,7 +87,9 @@ x$symbols  <- case_when(x$symbols.child. == "symbols" ~ 1,
                         x$symbols.child. == "without symbols" ~ 2)
 
 x$frustration <-  case_when(x$frustration.child. == "positive/neutral(c)" ~ 2,
-                            x$frustration.child. == "frustrated(c)" ~ 1)
+                            x$frustration.child. == "frustrated(c)" ~ 1,
+                            x$frustration.child. == "positive/neutral" ~ 2,
+                            x$frustration.child. == "frustrated" ~ 1)
 
 x$qual_beh <- case_when(x$qualifying == "soothing" ~ 3, 
                         x$qualifying == "none" ~ 4,
